06e838010a4544f641bd3f3d890a60e5c1dd3bdb
shahzebAM <shahzebali654321@gmail.com>
2022-02-02 09:42:41 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import arr from './upload';
+
+const MappingCard = () => {
+
+  return (
+
+    <div className='main'>
+      {arr.map((value) => (
+
+        <Card className={'mycard'} style={{ backgroundColor: '#403F4C', marginTop: 20}} variant='outlined' sx={{ maxWidth: '15rem', marginLeft: 'auto', marginRight: 'auto' }}>
+          <CardContent style={{backgroundColor: '#6096BA', padding: 0}}>
+            <Typography style={{color: '#a9a9a9', backgroundColor: '#403F4C', fontWeight: 'bold'}} align='center' gutterBottom variant="h6" component="div">
+              {value.name}
+            </Typography>
+
+          </CardContent>
+          
+          <CardMedia style={{backgroundColor: '#403F4C'}}
+          >
+            <iframe frameborder="0" allow="autoplay" allowfullscreen="true" style={{ width: '100%', height: '100%' }} src={value.src}></iframe>
+          </CardMedia>
+          
+          <CardActions style={{ justifyContent: 'center', backgroundColor: '#403F4C' }}>
+            <Button variant='contained' style={{backgroundColor: '#2C2B3C'}} size="small" href={value.link}>Download</Button>
+            
+          </CardActions>
+        </Card>
+
+      ))}
+
+    </div>
+  )
+}
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {MappingCard()}
     </div>
-  );
-}
 
+  )
+}
 export default App;
+
